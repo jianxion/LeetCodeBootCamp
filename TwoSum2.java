3c350516b4ce9c6a27147a4a2a5526f312fd271a
jianxion <sjxshen@ucdavis.edu>
2025-09-30 17:09:30 -0400

    first commit

---
+++ TwoSum2.java
@@ -0,0 +1,23 @@
+public class TwoSum2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args) {
+        TwoSum2 solution = new TwoSum2();
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(numbers, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
